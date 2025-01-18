d9df9b4d9dfa150fc1a2798dfc3efa8f7354c61e
Your updalla-apshir <updallaapshir@gmail.com>
2025-01-18 09:08:12 +0300

    Deployment

---
+++ src/context.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const AppContext = createContext();
+
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme:dark)'
+  ).matches;
+  const storedDarkMode = localStorage.getItem('darkTheme') === 'true';
+  const isDarkTheme = storedDarkMode || prefersDarkMode;
+  document.body.classList.toggle('dark-theme', isDarkTheme);
+  return isDarkTheme;
+};
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  const [searchTerm, setSearchTerm] = useState('cat');
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
